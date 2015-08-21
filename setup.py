34883534bf94158931e571a763ccfe49d7244347
Ricardo Cabral <ricardo@isnotworking.com>
2015-08-21 11:10:29 -0300

    bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pylibratometrics',
-    version='0.2',
+    version='0.3',
     packages=['pylibratometrics',],
     license='MIT License',
     long_description=open('README.md').read(),
     author = "Ricardo Niederberger Cabral",
     author_email = "ricardo@isnotworking.com",
     description = "Simple script to send psutil (host network/cpu/disk) stats into Librato",
-    download_url = 'https://github.com/digesto/pylibratometrics/tarball/0.2',
+    download_url = 'https://github.com/digesto/pylibratometrics/tarball/0.3',
     keywords = "librato metrics",
     url = "https://github.com/digesto/pylibratometrics",
     install_requires = ["requests", "psutil"],
